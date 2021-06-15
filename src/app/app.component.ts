19abe901b61b8c20dae46e4fa0cc30c3b0b974c8
idoia.castilla <idoia.castilla@inycom.es>
2021-06-15 09:43:34 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'iny-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'inycom';
+}
